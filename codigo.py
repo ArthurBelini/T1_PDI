e1692a13ae86a38b7960bcba22de954693fc2329
Arthur Belini Pini <arthur15942@gmail.com>
2023-09-03 15:53:17 -0300

    refatoracao

--- codigo.py
+++ codigo.py
@@ -29,26 +29,26 @@ hsv_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
 
 
 # Processamento
-mask_abs = cv2.inRange(  # Faixa de valores padrao
+mask_aux = cv2.inRange(  # Faixa de valores padrao
     hsv_image, (lower_aux, 0, 0), (upper_aux, 255, 255))
 
 if lower_abs < 0:  # Caso onde faixa de valores passa negativamente de 0
-    low_up = lower_abs % 180  # Limite inferior ou superior da faixa excecao
-    mask_aux = cv2.inRange(  # Mascara da faixa excecao
-        hsv_image, (low_up, 0, 0), (179, 255, 255))
-    hsv_image[mask_aux > 0, 0] = \
-        hsv_image[mask_aux > 0, 0] - 90  # Inverter faixa excecao
+    lim_abs = lower_abs % 180  # Limite inferior ou superior da faixa excecao
+    mask_abs = cv2.inRange(  # Mascara da faixa excecao
+        hsv_image, (lim_abs, 0, 0), (179, 255, 255))
     hsv_image[mask_abs > 0, 0] = \
-        hsv_image[mask_abs > 0, 0] + 90  # Inverter faixa padrao
+        hsv_image[mask_abs > 0, 0] - 90  # Inverter faixa excecao
+    hsv_image[mask_aux > 0, 0] = \
+        hsv_image[mask_aux > 0, 0] + 90  # Inverter faixa padrao
 
 elif upper_abs > 179:  # Caso onde faixa de valores passa de 179
-    low_up = upper_abs % 180  # Limite inferior ou superior da faixa excecao
-    mask_aux = cv2.inRange(  # Mascara da faixa excecao
-        hsv_image, (0, 0, 0), (low_up, 255, 255))
-    hsv_image[mask_aux > 0, 0] = \
-        hsv_image[mask_aux > 0, 0] + 90  # Inverter faixa excecao
+    lim_abs = upper_abs % 180  # Limite inferior ou superior da faixa excecao
+    mask_abs = cv2.inRange(  # Mascara da faixa excecao
+        hsv_image, (0, 0, 0), (lim_abs, 255, 255))
     hsv_image[mask_abs > 0, 0] = \
-        hsv_image[mask_abs > 0, 0] - 90  # Inverter faixa padrao
+        hsv_image[mask_abs > 0, 0] + 90  # Inverter faixa excecao
+    hsv_image[mask_aux > 0, 0] = \
+        hsv_image[mask_aux > 0, 0] - 90  # Inverter faixa padrao
     
 else:  # Caso onde somente faixa padrao possui valores
     mask_abs_lower = cv2.inRange(  # Parte da faixa padrao < 89
