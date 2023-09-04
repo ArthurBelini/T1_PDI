4e93b54923c590c727cfe8257c94876b4474ea56
Arthur Belini Pini <arthur15942@gmail.com>
2023-09-04 11:04:30 -0300

    refatoracao
    extraindo funcao de inverter faixas

--- codigo.py
+++ codigo.py
@@ -30,36 +30,29 @@ hsv_channel = hsv_image[:, :, 0]
 
 
 # Processamento
-mask_aux = cv2.inRange(  # Faixa de valores padrao
-    hsv_channel, lower_aux, upper_aux)
-
 if lower_abs < 0:  # Caso onde faixa de valores passa negativamente de 0
     lim_abs = lower_abs % 180  # Limite inferior ou superior da faixa excecao
-    mask_abs = cv2.inRange(  # Mascara da faixa excecao
-        hsv_channel, lim_abs, 179)
-    hsv_channel[mask_abs > 0] = \
-        hsv_channel[mask_abs > 0] - 90  # Inverter faixa excecao
-    hsv_channel[mask_aux > 0] = \
-        hsv_channel[mask_aux > 0] + 90  # Inverter faixa padrao
+    hsv_channel = \
+        invert_range(hsv_channel, lim_abs, 179, -1)  # Inverter faixa excecao
+    hsv_channel = \
+        invert_range(hsv_channel, lower_aux, upper_aux)  # Inverter faixa padrao
 
 elif upper_abs > 179:  # Caso onde faixa de valores passa de 179
     lim_abs = upper_abs % 180  # Limite inferior ou superior da faixa excecao
-    mask_abs = cv2.inRange(  # Mascara da faixa excecao
-        hsv_channel, 0, lim_abs)
-    hsv_channel[mask_abs > 0] = \
-        hsv_channel[mask_abs > 0] + 90  # Inverter faixa excecao
-    hsv_channel[mask_aux > 0] = \
-        hsv_channel[mask_aux > 0] - 90  # Inverter faixa padrao
+    hsv_channel = \
+        invert_range(hsv_channel, 0, lim_abs)  # Inverter faixa excecao
+    hsv_channel = \
+        invert_range(hsv_channel, lower_aux, upper_aux, -1)  # Inverter faixa padrao
     
 else:  # Caso onde somente faixa padrao possui valores
-    mask_abs_lower = cv2.inRange(  # Parte da faixa padrao < 89
+    mask_lower = cv2.inRange(  # Parte da faixa padrao < 89
         hsv_channel, lower_abs, min(upper_abs, 89))
-    mask_abs_upper = cv2.inRange(  # Parte da faixa padrao >= 90
+    mask_upper = cv2.inRange(  # Parte da faixa padrao >= 90
         hsv_channel, max(lower_abs, 90), upper_abs)
-    hsv_channel[mask_abs_lower > 0] = \
-        hsv_channel[mask_abs_lower > 0] + 90 # Inverter faixa padrao < 89
-    hsv_channel[mask_abs_upper > 0] = \
-        hsv_channel[mask_abs_upper > 0] - 90 # Inverter faixa padrao >= 90
+    hsv_channel[mask_lower > 0] = \
+        hsv_channel[mask_lower > 0] + 90 # Inverter faixa padrao < 89
+    hsv_channel[mask_upper > 0] = \
+        hsv_channel[mask_upper > 0] - 90 # Inverter faixa padrao >= 90
 
 
 # Finalização
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import cv2
+import numpy as np
+
 def ler(x, y):
     while True:
         z = int(input(f"Digite um numero de {x}-{y}: "))
@@ -5,3 +8,9 @@ def ler(x, y):
             break
 
     return z
+
+def invert_range(channel, lower, upper, sign=1):
+    mask = cv2.inRange(channel, lower, upper)
+    channel[mask > 0] = channel[mask > 0] + sign * 90
+
+    return channel
