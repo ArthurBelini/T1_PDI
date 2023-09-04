807bab5bec1b1ffeda95c4eb512bf251cd3d7ec0
Arthur Belini Pini <arthur15942@gmail.com>
2023-09-04 10:09:25 -0300

    refatoracao
    tratamento do canal hue isolado

--- codigo.py
+++ codigo.py
@@ -26,42 +26,44 @@ if not os.path.isfile(image_path):
 
 original_image = cv2.imread(image_path)
 hsv_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
+hsv_channel = hsv_image[:, :, 0]
 
 
 # Processamento
 mask_aux = cv2.inRange(  # Faixa de valores padrao
-    hsv_image, (lower_aux, 0, 0), (upper_aux, 255, 255))
+    hsv_channel, lower_aux, upper_aux)
 
 if lower_abs < 0:  # Caso onde faixa de valores passa negativamente de 0
     lim_abs = lower_abs % 180  # Limite inferior ou superior da faixa excecao
     mask_abs = cv2.inRange(  # Mascara da faixa excecao
-        hsv_image, (lim_abs, 0, 0), (179, 255, 255))
-    hsv_image[mask_abs > 0, 0] = \
-        hsv_image[mask_abs > 0, 0] - 90  # Inverter faixa excecao
-    hsv_image[mask_aux > 0, 0] = \
-        hsv_image[mask_aux > 0, 0] + 90  # Inverter faixa padrao
+        hsv_channel, lim_abs, 179)
+    hsv_channel[mask_abs > 0] = \
+        hsv_channel[mask_abs > 0] - 90  # Inverter faixa excecao
+    hsv_channel[mask_aux > 0] = \
+        hsv_channel[mask_aux > 0] + 90  # Inverter faixa padrao
 
 elif upper_abs > 179:  # Caso onde faixa de valores passa de 179
     lim_abs = upper_abs % 180  # Limite inferior ou superior da faixa excecao
     mask_abs = cv2.inRange(  # Mascara da faixa excecao
-        hsv_image, (0, 0, 0), (lim_abs, 255, 255))
-    hsv_image[mask_abs > 0, 0] = \
-        hsv_image[mask_abs > 0, 0] + 90  # Inverter faixa excecao
-    hsv_image[mask_aux > 0, 0] = \
-        hsv_image[mask_aux > 0, 0] - 90  # Inverter faixa padrao
+        hsv_channel, 0, lim_abs)
+    hsv_channel[mask_abs > 0] = \
+        hsv_channel[mask_abs > 0] + 90  # Inverter faixa excecao
+    hsv_channel[mask_aux > 0] = \
+        hsv_channel[mask_aux > 0] - 90  # Inverter faixa padrao
     
 else:  # Caso onde somente faixa padrao possui valores
     mask_abs_lower = cv2.inRange(  # Parte da faixa padrao < 89
-        hsv_image, (lower_abs, 0, 0), (min(upper_abs, 89), 255, 255))
+        hsv_channel, lower_abs, min(upper_abs, 89))
     mask_abs_upper = cv2.inRange(  # Parte da faixa padrao >= 90
-        hsv_image, (max(lower_abs, 90), 0, 0), (upper_abs, 255, 255))
-    hsv_image[mask_abs_lower > 0, 0] = \
-        hsv_image[mask_abs_lower > 0, 0] + 90 # Inverter faixa padrao < 89
-    hsv_image[mask_abs_upper > 0, 0] = \
-        hsv_image[mask_abs_upper > 0, 0] - 90 # Inverter faixa padrao >= 90
+        hsv_channel, max(lower_abs, 90), upper_abs)
+    hsv_channel[mask_abs_lower > 0] = \
+        hsv_channel[mask_abs_lower > 0] + 90 # Inverter faixa padrao < 89
+    hsv_channel[mask_abs_upper > 0] = \
+        hsv_channel[mask_abs_upper > 0] - 90 # Inverter faixa padrao >= 90
 
 
 # Finalização
+hsv_image[:, :, 0] = hsv_channel
 rgb_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
 
 cv2.imshow('New Image', rgb_image)
